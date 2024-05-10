687bb62f1f86255db2c6147d545fe9e97239dc5e
koinoniays2 <koinonia_ys@naver.com>
2024-05-10 10:52:57 +0900

    [update] 쿠키

--- app/login/action.js
+++ app/login/action.js
@@ -16,20 +16,23 @@ export async function login(formData) {
     },
     select: { // 필요한 필드만 가져오는 것
       id: true,
-      password: true
+      password: true,
+      // username: true
     }
   });
 
   if(user) { // 입력된 패스워드, db 패스워드
     const ok = await bcrypt.compare(password, user.password); // bcrypt는 await
 
-    if(ok) { // 서버 쿠키
+    if(ok) { // 쿠키
       const session = await getIronSession(cookies(), {
-        cookieName: "cococookie",
+        cookieName: "cococookie", // 쿠키 이름 설정
         password: process.env.COOKIE_PASSWORD // 40자 이상 비밀번호
       });
-      session.id = user.id;
-      await session.save();
+      session.id = user.id; // 세션(쿠키) 객체에 id 넣기
+      // session.nmae = user.username; // test
+      console.log(session);
+      await session.save(); // 쿠키 저장하기
 
       return redirect("/settings");
     }
